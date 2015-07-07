f3e90b03755d0a4850761dacae8b2b3089652682
Rodrigo García <yo@rodrigogarcia.me>
2015-07-07 10:41:27 +0200

    Updates script to allow easier execution of experiments

--- script/run_experiments.bat
+++ script/run_experiments.bat
@@ -2,16 +2,21 @@
 
 SET APEX_BIN="C:\Program Files (x86)\apex\bin\apex.exe"
 
-ECHO "Running training phase..."
+SET EXPERIMENTS_ROOT=..\experiment\
+SET EXPERIMENTS_FILES=AM-fm.apx FM-fm.apx
+
+SET /P SUBJECT="Please enter your name: "
+
+ECHO Running training phase...
 START /W "" %APEX_BIN% -an "..\experiment\training.apx"
 
-ECHO "Running AM-fm experiment..."
-START /W "" %APEX_BIN% "..\experiment\AM-fm.apx"
+FOR %%f IN (%EXPERIMENTS_FILES%) DO (
+    ECHO Running experiment %%f...
+    START /W "" %APEX_BIN% "%EXPERIMENTS_ROOT%%%f"
+    PAUSE
+)
 
-ECHO "Running FM-fm experiment..."
-START /W "" %APEX_BIN% "..\experiment\FM-fm.apx"
-
-START /W move_results_files.bat
+START /W move_results_files.bat %SUBJECT%
 
 EXIT
 
