8f8f875ee9c9ebadff9cf610c91e34935cdead02
Rodrigo García <yo@rodrigogarcia.me>
2015-07-08 16:42:58 +0200

    Adds separate training phases

--- script/run_am_experiments.bat
+++ script/run_am_experiments.bat
@@ -1,8 +1,21 @@
 @ECHO OFF
 
+SET APEX_BIN="C:\Program Files (x86)\apex\bin\apex.exe"
+SET EXPERIMENTS_ROOT=..\experiment\
 SET EXPERIMENTS_FILES=AM-fm.apx AM-fc.apx AM-md.apx AM-SPL.apx
 
-START "" run_experiments.bat %EXPERIMENTS_FILES%
+SET /P SUBJECT="Please enter your name: "
+
+ECHO Running training phase...
+START /W "" %APEX_BIN% -an "..\experiment\AM-training.apx"
+PAUSE
+
+FOR %%G IN (%EXPERIMENTS_FILES%) DO (
+    ECHO Running experiment %%G...
+    START /W "" %APEX_BIN% "%EXPERIMENTS_ROOT%%%G"
+    PAUSE
+)
+
+START /W "" move_results_files.bat %SUBJECT%
 
 EXIT
-
--- script/run_fm_experiments.bat
+++ script/run_fm_experiments.bat
@@ -1,7 +1,21 @@
 @ECHO OFF
 
-SET EXPERIMENTS_FILES=FM-fm.apx FM-fc.apx FM-md.apx FM-SPL.apx
+SET APEX_BIN="C:\Program Files (x86)\apex\bin\apex.exe"
+SET EXPERIMENTS_ROOT=..\experiment\
+SET EXPERIMENTS_FILES=FM-fm.apx FM-fc.apx FM-df.apx FM-SPL.apx
 
-START "" run_experiments.bat %EXPERIMENTS_FILES%
+SET /P SUBJECT="Please enter your name: "
+
+ECHO Running training phase...
+START /W "" %APEX_BIN% -an "..\experiment\FM-training.apx"
+PAUSE
+
+FOR %%G IN (%EXPERIMENTS_FILES%) DO (
+    ECHO Running experiment %%G...
+    START /W "" %APEX_BIN% "%EXPERIMENTS_ROOT%%%G"
+    PAUSE
+)
+
+START /W "" move_results_files.bat %SUBJECT%
 
 EXIT
