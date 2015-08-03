719c68f62d3f9ed6a54edc2b9b20f376d5309465
Rodrigo García <yo@rodrigogarcia.me>
2015-08-03 19:23:47 +0200

    Splits frequency modulation experiments into two

--- script/run_am_experiments.bat
+++ script/run_am_experiments.bat
@@ -1,17 +1,16 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET EXPERIMENTS_FILES_1=AM-fm.apx AM-fc.apx AM-md.apx AM-SPL.apx
-SET EXPERIMENTS_FILES_2=AM-fc.apx AM-md.apx AM-SPL.apx AM-fm.apx
-SET EXPERIMENTS_FILES_3=AM-md.apx AM-SPL.apx AM-fm.apx AM-fc.apx
-SET EXPERIMENTS_FILES_4=AM-SPL.apx AM-fm.apx AM-fc.apx AM-fm.apx
+SET EXPERIMENTS_FILES_1=AM-fm.apx AM-fc.apx AM-md.apx AM-fm.apx AM-SPL.apx
+SET EXPERIMENTS_FILES_2=AM-fc.apx AM-md.apx AM-fm.apx AM-SPL.apx AM-fm.apx
+SET EXPERIMENTS_FILES_3=AM-md.apx AM-fm.apx AM-SPL.apx AM-fm.apx AM-fc.apx
+SET EXPERIMENTS_FILES_4=AM-SPL.apx AM-fm.apx AM-fc.apx AM-md.apx AM-fm.apx
 
-SET /P CONDITION="Enter condition: "
+SET /P CONDITION="Condition: "
+SET /P ID="Id: "
 CLS
 
-SET /P SUBJECT="Enter subject's name: "
-
 START /W "" run_experiments.bat !EXPERIMENTS_FILES_%CONDITION%!
-START /W "" move_results_files.bat %SUBJECT%
+START /W "" move_results_files.bat %ID%
 
 EXIT
--- script/run_fm_experiments.bat
+++ script/run_fm_experiments.bat
@@ -1,17 +1,16 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET EXPERIMENTS_FILES_1=FM-fm.apx FM-fc.apx FM-df.apx FM-SPL.apx
-SET EXPERIMENTS_FILES_2=FM-fc.apx FM-df.apx FM-SPL.apx FM-fm.apx
-SET EXPERIMENTS_FILES_3=FM-df.apx FM-SPL.apx FM-fm.apx FM-fc.apx
-SET EXPERIMENTS_FILES_4=FM-SPL.apx FM-fm.apx FM-fc.apx FM-df.apx
+SET EXPERIMENTS_FILES_1=FM-fm.apx FM-fc.apx FM-df.apx FM-fm.apx FM-SPL.apx
+SET EXPERIMENTS_FILES_2=FM-fc.apx FM-df.apx FM-fm.apx FM-SPL.apx FM-fm.apx
+SET EXPERIMENTS_FILES_3=FM-df.apx FM-fm.apx FM-SPL.apx FM-fm.apx FM-fc.apx
+SET EXPERIMENTS_FILES_4=FM-SPL.apx FM-fm.apx FM-fc.apx FM-df.apx FM-fm.apx
 
-SET /P CONDITION="Enter condition: "
+SET /P CONDITION="Condition: "
+SET /P ID="Id: "
 CLS
 
-SET /P SUBJECT="Enter subject's name: "
-
 START /W "" run_experiments.bat !EXPERIMENTS_FILES_%CONDITION%!
-START /W "" move_results_files.bat %SUBJECT%
+START /W "" move_results_files.bat %ID%
 
 EXIT
